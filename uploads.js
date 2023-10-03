bc23a8494b1709f7272f0688cf4ff5ce323e6db6
G-RIMA <gikandimaria178@gmail.com>
2023-10-03 12:54:03 +0300

    installed dotenv

--- uploads.js
+++ uploads.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const mongoURI = process.env.DATABASE_URL;
+const mongoURL = process.env.DATABASE_URL;
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Create a schema and model for video documents
 const videoSchema = new mongoose.Schema({
